c9dff180294d4932bb0073e11a5ca1f97b2c6170
Ramis <ramismubick@gmail.com>
2024-03-15 01:06:19 +0300

    Компонент для получения случайного факта с применением TanStack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { FC } from "react"
+import { FC, useMemo } from "react"
+import { Tabs } from "antd"
+
+import RandomFactForm from "./components/RandomFactForm/RandomFactForm"
 
 const App: FC = () => {
+  const TabItems = useMemo(() => {
+    return [
+      {
+        key: 'random-fact',
+        label: 'Get a random fact',
+        children: <RandomFactForm />
+      }
+    ]
+  }, [])
+
   return (
-    <>
-      <div>Vite + React + TS</div>
-    </>
+    <Tabs
+      type="card"
+      centered
+      size="large"
+      items={TabItems}
+    />
   )
 }
 
---
+++ src/components/RandomFactForm/RandomFactForm.tsx
@@ -0,0 +1,47 @@
+import { FC, useCallback } from "react"
+import { useQuery } from "@tanstack/react-query"
+import { Button, Flex, Input } from "antd"
+
+interface FactResponse {
+    fact: string;
+    length: number;
+}
+
+const RandomFactForm: FC = () => {
+    const fetchRandomFact = useCallback(async () => {
+        const response = await fetch('https://catfact.ninja/fact')
+        if (!response.ok) {
+            throw new Error('Sorry, an error occured')
+        }
+        const data = await response.json()
+        if (!data.fact) {
+            throw new Error('Sorry, an error occured')
+        }
+        return data as FactResponse
+    }, [])
+
+    const { isLoading, isError, error, data, refetch } = useQuery<FactResponse>({
+        queryKey: ['fact'],
+        queryFn: fetchRandomFact,
+        retry: false,
+        enabled: false
+    })
+
+    return (
+        <Flex
+            align="center"
+            gap="middle">
+            <Button
+                type="primary"
+                onClick={() => refetch()}>
+                Get a random fact about cats
+            </Button>
+            <Input.TextArea
+                readOnly
+                value={isLoading ? 'Loading...' : isError ? error.message : data?.fact || ''}
+            />
+        </Flex>
+    )
+}
+
+export default RandomFactForm
